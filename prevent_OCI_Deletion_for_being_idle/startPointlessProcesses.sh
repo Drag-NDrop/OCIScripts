9f9c69a35b54276695cbbd055ee8a1f983b90692
sabinM1 <sabin@disroot.org>
2023-04-08 01:34:49 +0300

    Optimize code for Standard.E2.1.Micro and clean up
    Optimized for Oracle Cloud VM.Standard.E2.1.Micro

--- prevent_OCI_Deletion_for_being_idle/startPointlessProcesses.sh
+++ prevent_OCI_Deletion_for_being_idle/startPointlessProcesses.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
+
+# Run this script using a "screen" (# apt install screen) ($ man screen)
+
 cd "$(dirname "$0")" || exit
 
-/bin/bash cpuUser.sh & disown
-
-x=0
 currentCpuLoad=$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]
 
 while :
 do
-  if [ $currentCpuLoad -le 10 ]
+  if [ $currentCpuLoad -le 20 ]
   then
-    while [ $x -le 10 ]
-    do
-      #for memory drain enable this instead     /bin/bash ./cpuUserArray.sh & disown
-      /bin/bash cpuUser.sh & disown
-      x=$(( $x + 1 ))
-    done
-    x=0
+    /bin/bash cpuUser.sh
     currentCpuLoad=$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]
-    echo $currentCpuLoad
+#    echo $currentCpuLoad
   fi
 done
