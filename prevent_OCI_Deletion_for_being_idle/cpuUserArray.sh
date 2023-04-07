7a0bd1ce040a0e34f5638eaff8ca5e70471e4674
Drag-NDrop <110815341+Drag-NDrop@users.noreply.github.com>
2023-04-07 02:28:11 +0200

    Editted cosmetics

--- prevent_OCI_Deletion_for_being_idle/cpuUserArray.sh
+++ prevent_OCI_Deletion_for_being_idle/cpuUserArray.sh
@@ -6,12 +6,9 @@ declare -A pointlessNumbersArray
 while [ $x -le 100000 ]
 do
   currentArrayLength=${#pointlessNumbersArray[@]}
-#  echo "current length $currentArrayLength"
   newArrayLength=$(( currentArrayLength + 1 ))
-#  echo "new array length: $newArrayLength"
   pointlessNumbersArray[$newArrayLength]+=$(( $RANDOM % 5000000000000 + 100000000000000000000 ))
   x=$(( $x + 1 ))
-#  echo "added another number. The amount of numbers is now ${#pointlessNumbersArray[@]}"
 done
 
 end=$(TZ=UTC date --date now-$now +%H:%M:%S.%N)
--- prevent_OCI_Deletion_for_being_idle/startPointlessProcesses.sh
+++ prevent_OCI_Deletion_for_being_idle/startPointlessProcesses.sh
@@ -21,5 +21,5 @@ log_size_kb=`du -k /home/ubuntu/prevent_OCI_Deletion_for_being_idle/log/trackPoi
 if [ $log_size_kb -ge 1000 ]
 then
   echo "" > /home/ubuntu/prevent_OCI_Deletion_for_being_idle/log/trackPointlessWork.log
-  echo "cleaned logfile"
+  echo "CPU Waster solution:: cleaned logfile"
 fi
